fa60fc65df31681e89e95c3208ff2783de7a5d17
Fabiana Quelott <56900077+Fabianaquelott@users.noreply.github.com>
2024-08-14 09:31:54 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
             sof.inserir(new Aluno("Fabiana Quelott", 119214091, 25, "Rua dos Amendoins"));
             sof.inserir(new Aluno("Lucas Vilas", 1, 21, "Rua dos Tupis"));
             sof.inserir(new Aluno("Marlon Santos", 1, 22, "Rua dos Tupiniquins"));
-            sof.inserir(new Aluno("Lola Solano", 1, 23, "Rua dos Guajajaras"));
+            sof.inserir(new Aluno("Lua Sol", 1, 23, "Rua dos Guajajaras"));
             sof.inserir(new Aluno("Hercules Splin", 1, 35, "Rua São Paulo"));
             sof.inserir(new Aluno("Olivia Santos", 1, 15, "Rua Rio de Janeiro"));
     }
